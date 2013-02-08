67a568c7edfaed5a6b7c00202399b89b535bbee1
fernando.bs.dark <fernando.bs.dark@gmail.com>
2013-02-08 10:55:56 +0000

    identação

--- classes/util/FormataActions.class.php
+++ classes/util/FormataActions.class.php
@@ -2,19 +2,19 @@
 
 class FormataActions {
 
-	private $objCrypt;
+    private $objCrypt;
 
     public function __construct() {
-		$this->objCrypt = new Cryptografia();
+        $this->objCrypt = new Cryptografia();
     }
 
-    private function getObjCrypt(){
-    	if($this->objCrypt == null)
-    		$this->objCrypt = new Cryptografia();
-    	return $this->objCrypt;
+    private function getObjCrypt() {
+        if ($this->objCrypt == null)
+            $this->objCrypt = new Cryptografia();
+        return $this->objCrypt;
     }
 
-	/**
+    /**
      * Método default de criação de actions para o grid
      *
      * @author André Coura
@@ -26,32 +26,32 @@ class FormataActions {
      * @param String $tipo: tipo de parametro a ser setado
      * @param String $strCategoria: caso seja de alguma categoria, é setado aqui
      * @return String $strLink: o html do link criado //$strLink = "<a href=\"javascript:void(vaiPara('?".$tipo."=".$link."'))\">".$label."</a>";
-	 * 
-	 * 
+     * 
+     * 
      * @author Matheus Vieira
      * @since 2.1 - 15/12/2011
      * @return String $strLink: o html do link criado direto no href, desta forma o botão voltar do navegador funciona.
      */
-	public function gridAction($data,$value,$classe,$label,$tipo = "c",$strCategoria = "",$param1="",$valParam1="",$param2="",$valParam2="",$xmlParam="",$pag="",$filtros="",$buscaGrid=""){
-		$strParam = "";
-		$strParam3 = "";
-		if($param1 !="" && $valParam1!="")
-			$strParam = "&".$param1."=".$valParam1;
-		if($param2 !="" && $valParam2!="")
-			$strParam2 = "&".$param2."=".$valParam2;
-		if($pag !="")
-			$strParam3 .="&pag=".$pag;
-		if($filtros !="")
-			$strParam3 .="&filtros=".$filtros;
-		if($buscaGrid !="")
-			$strParam3 .=$buscaGrid;
-		
-		$link = self::getObjCrypt()->cryptData(($strCategoria!=""?$strCategoria."&f=":"").$classe."&".$value."&id=".$data.$strParam.$strParam2.$xmlParam.$strParam3);
-		$strLink = "<a href=\"?".$tipo."=".$link."\">".$label."</a>";
-		return $strLink;
+    public function gridAction($data, $value, $classe, $label, $tipo = "c", $strCategoria = "", $param1 = "", $valParam1 = "", $param2 = "", $valParam2 = "", $xmlParam = "", $pag = "", $filtros = "", $buscaGrid = "") {
+        $strParam = "";
+        $strParam3 = "";
+        if ($param1 != "" && $valParam1 != "")
+            $strParam = "&" . $param1 . "=" . $valParam1;
+        if ($param2 != "" && $valParam2 != "")
+            $strParam2 = "&" . $param2 . "=" . $valParam2;
+        if ($pag != "")
+            $strParam3 .="&pag=" . $pag;
+        if ($filtros != "")
+            $strParam3 .="&filtros=" . $filtros;
+        if ($buscaGrid != "")
+            $strParam3 .=$buscaGrid;
+
+        $link = self::getObjCrypt()->cryptData(($strCategoria != "" ? $strCategoria . "&f=" : "") . $classe . "&" . $value . "&id=" . $data . $strParam . $strParam2 . $xmlParam . $strParam3);
+        $strLink = "<a href=\"?" . $tipo . "=" . $link . "\">" . $label . "</a>";
+        return $strLink;
     }
 
-	/**
+    /**
      * Método default de criação de confirms para o grid
      *
      * @author André Coura
@@ -64,19 +64,19 @@ class FormataActions {
      * @param String $strCategoria: caso seja de alguma categoria, é setado aqui
      * @return String $strLink: o html do link criado
      */
-	public function gridConfirm($data,$value,$classe,$label,$mensConfirm,$tipo = "c",$strCategoria = "",$param1="",$valParam1="",$param2="",$valParam2=""){
-		$strParam = "";
-		if($param1 !="" && $valParam1!="")
-			$strParam = "&".$param1."=".$valParam1;
-		if($param2 !="" && $valParam2!="")
-			$strParam2 = "&".$param2."=".$valParam2;
+    public function gridConfirm($data, $value, $classe, $label, $mensConfirm, $tipo = "c", $strCategoria = "", $param1 = "", $valParam1 = "", $param2 = "", $valParam2 = "") {
+        $strParam = "";
+        if ($param1 != "" && $valParam1 != "")
+            $strParam = "&" . $param1 . "=" . $valParam1;
+        if ($param2 != "" && $valParam2 != "")
+            $strParam2 = "&" . $param2 . "=" . $valParam2;
 
-		$link = self::getObjCrypt()->cryptData(($strCategoria!=""?$strCategoria."&f=":"").$classe."&".$value."&id=".$data.$strParam.$strParam2);
-		$strLink = "<a href=\"javascript:void(confirmIr('?".$tipo."=".$link."','".$mensConfirm."'))\">".$label."</a>";
-		return $strLink;
+        $link = self::getObjCrypt()->cryptData(($strCategoria != "" ? $strCategoria . "&f=" : "") . $classe . "&" . $value . "&id=" . $data . $strParam . $strParam2);
+        $strLink = "<a href=\"javascript:void(confirmIr('?" . $tipo . "=" . $link . "','" . $mensConfirm . "'))\">" . $label . "</a>";
+        return $strLink;
     }
 
-	/**
+    /**
      * Método de criação de impressao para o grid
      *
      * @author Matheus Vieira
@@ -89,18 +89,19 @@ class FormataActions {
      * @param String $strCategoria: caso seja de alguma categoria, é setado aqui
      * @return String $strLink: o html do link criado
      */
-	public function gridImprime($data,$value,$classe,$label,$tipo = "c",$strCategoria = "",$param1="",$valParam1=""){
-		$strParam = "";
-		if($param1 !="" && $valParam1!="")
-			$strParam = "&".$param1."=".$valParam1;
-		$link = (($strCategoria!=""?$strCategoria."&f=":"").$classe."&".$value."&id=".$data.$strParam);
-		$value = end(explode("a=", $value));
-		$link2 = self::getObjCrypt()->cryptData(SERVIDOR_FISICO."modulos/inventario/classes/view/&classe=".$classe."&metodo=".$value."&id=".$data);
-		
-		
-		$strLink = "<a href=\"javascript:void(imprimir('".$link2."'))\">".$label."</a>";
-		return $strLink;
+    public function gridImprime($data, $value, $classe, $label, $tipo = "c", $strCategoria = "", $param1 = "", $valParam1 = "") {
+        $strParam = "";
+        if ($param1 != "" && $valParam1 != "")
+            $strParam = "&" . $param1 . "=" . $valParam1;
+        $link = (($strCategoria != "" ? $strCategoria . "&f=" : "") . $classe . "&" . $value . "&id=" . $data . $strParam);
+        $value = end(explode("a=", $value));
+        $link2 = self::getObjCrypt()->cryptData(SERVIDOR_FISICO . "modulos/inventario/classes/view/&classe=" . $classe . "&metodo=" . $value . "&id=" . $data);
+
+
+        $strLink = "<a href=\"javascript:void(imprimir('" . $link2 . "'))\">" . $label . "</a>";
+        return $strLink;
     }
 
 }
+
 ?>
