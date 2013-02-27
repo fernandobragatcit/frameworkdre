704e71aa3de3e5b52b8ec13d73ada17e6bc2390f
fernando.bs.dark <fernando.bs.dark@gmail.com>
2013-02-27 13:27:26 +0000

    Resolvido Problema de segurança Tempo Gasto: 3 horas.

--- classes/util/FormataPost.class.php
+++ classes/util/FormataPost.class.php
@@ -108,6 +108,13 @@ class FormataPost {
         }
         return $arrResult;
     }
+    public static function anti_injection($sql) {
+        $sql = preg_replace(sql_regcase("/(from|select|insert|delete|where|drop table|show tables|#|\*|--|\\\\)/"), "", $sql);
+        $sql = trim($sql); 
+        $sql = strip_tags($sql);
+        $sql = addslashes($sql);
+        return $sql;
+    }
 
 }
 
